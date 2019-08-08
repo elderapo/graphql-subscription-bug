09a505c5c47fff393885398a51c8926a7754f2ef
Tomasz Martyński <tomek@martynski.pl>
2019-08-08 19:24:16 +0200

    convert from `type-graphql` to pure `apollo-server` with `graphql-subscriptions`

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,62 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server";
-import { buildSchema } from "type-graphql";
+import { PubSub } from "graphql-subscriptions";
 
-import { SampleResolver } from "./resolver";
+const pubsub = new PubSub();
+
+const resolvers = {
+  Query: {
+    currentDate: {
+      resolve: () => {
+        return new Date();
+      }
+    }
+  },
+  Subscription: {
+    normalSubscription: {
+      resolve: (root: any) => {
+        return {
+          id: root.id,
+          message: root.message,
+          date: new Date()
+        };
+      },
+      subscribe: () => pubsub.asyncIterator("NOTIFICATION")
+    }
+  }
+};
+
+const typeDefs = `scalar DateTime
+
+type Notification {
+  id: ID!
+  message: String
+  date: DateTime!
+}
+
+type Query {
+  currentDate: DateTime!
+}
+
+type Subscription {
+  normalSubscription: Notification!
+}`;
 
 async function bootstrap() {
-  // Build the TypeGraphQL schema
-  const schema = await buildSchema({
-    resolvers: [SampleResolver]
-  });
-
-  // Create GraphQL server
   const server = new ApolloServer({
-    schema,
+    typeDefs,
+    resolvers,
     playground: true
-    // you can pass the endpoint path for subscriptions
-    // otherwise it will be the same as main graphql endpoint
-    // subscriptions: "/subscriptions",
   });
 
-  // Start the server
+  let notificationID: number = 0;
+  setInterval(() => {
+    pubsub.publish("NOTIFICATION", {
+      id: notificationID++,
+      message: "Some message..."
+    });
+  }, 500);
+
   const { url } = await server.listen(4000);
   console.log(`Server is running, GraphQL Playground available at ${url}`);
 }
